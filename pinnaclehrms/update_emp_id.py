969d3a6af53cbb7b9a1fb5d5ab9bd2d13f275f71
Piyush Shukla <piyushs@mygstcafe.in>
2025-01-24 17:31:16 +0530

    Delete pinnaclehrms directory

--- pinnaclehrms/update_emp_id.py
+++
@@ -1,6 +0,0 @@
-import frappe
-from hrms.hr.doctype.attendance.attendance import Attendance
-
-def custom_before_save(self, method):
-    empId = frappe.db.get_value('Employee', {'attendance_device_id': self.custom_attendance_device_id}, 'employee')
-    self.employee = empId
