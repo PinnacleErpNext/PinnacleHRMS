dacf402aed9d2ccb838d5a9e67742d1c9c9a5a35
Piyush Shukla <piyushs@mygstcafe.in>
2025-05-07 14:57:52 +0530

    Add attendance device option in attendance form.
    Date: 07-05-2025
    By: Piyush Shukla

--- pinnaclehrms/utility/update_emp_id.py
+++ pinnaclehrms/utility/update_emp_id.py
@@ -1,10 +1,22 @@
 import frappe
 from hrms.hr.doctype.attendance.attendance import Attendance
 
+
 def custom_before_save(self, method):
     if self.employee is None:
-        empId = frappe.db.get_value('Attendance Device ID Allotment', {'device_id': self.custom_attendance_device_id}, 'parent')
-        if(empId is None):
-            empId = frappe.db.get_value('Employee', {'attendance_device_id': self.custom_attendance_device_id}, 'employee')
-    
+        empId = frappe.db.get_value(
+            "Attendance Device ID Allotment",
+            {
+                "device_id": self.custom_attendance_device_id,
+                "device": self.custom_attendance_device,
+            },
+            "parent",
+        )
+        if empId is None:
+            empId = frappe.db.get_value(
+                "Employee",
+                {"attendance_device_id": self.custom_attendance_device_id},
+                "employee",
+            )
+
         self.employee = empId
